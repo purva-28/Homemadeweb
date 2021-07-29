4835f8fda417393dc681b48a09c32a0daad5c777
purva-28 <67964272+purva-28@users.noreply.github.com>
2021-07-29 19:22:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,15 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'localhost');
+/*define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'admin');
+define('DB_NAME', 'admin');*/
+
+define('DB_SERVER', 'sql6.freemysqlhosting.net');
+define('DB_USERNAME', 'sql6427360');
+define('DB_PASSWORD', 'xbmfxZ4wtu');
+define('DB_NAME', 'sql6427360');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -17,3 +22,4 @@ if($conn == false){
 }
 
 ?>
+
